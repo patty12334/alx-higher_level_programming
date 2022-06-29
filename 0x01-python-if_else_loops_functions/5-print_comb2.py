037a01f44729d5094907c3a7b2a9bf9e29923b9c
ugwu patiencechy@gmail.com <ugwupatiencechy@gmail.com>
2022-06-29 15:08:03 -0700

    corections 0n task 1 to 6

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/user/bin/python3
 for a in range (100):
-	if i == 99:
-		print(i)
+	if a == 99:
+		print(a)
 	else:
-		print("{}>format('0 + str(i)if i < 10 else i, end=','
+		print("{}>format('0 + str(a)if a < 10 else a, end=","
 	
