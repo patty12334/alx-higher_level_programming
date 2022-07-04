c553fdfff8274ae8faee1ddc6d494b992324bcf0
ugwu patiencechy@gmail.com <ugwupatiencechy@gmail.com>
2022-07-04 07:58:38 -0700

    python solution to tas 1-12 of python data structure

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+x_integer(matrix=[[]]):
+    if not matrix:
+        return
+    else:
+        for i in matrix:
+            for j in i:
+                if j != 0:
+                    print(" ", end='')
+                print("{:d}".format(j), end=" ")
+            print()i
