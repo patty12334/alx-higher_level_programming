6ab4a308d67644020d534f68b4a55b6e437ac578
ugwu patiencechy@gmail.com <ugwupatiencechy@gmail.com>
2022-07-08 01:34:54 -0700

    task 9 correction

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+	sub = 0
+	max_num = max(list_num)
+
+	for n in list_num:
+		if max_num > n:
+		sub += n
+	return (max_num -sub)
+
+def roman_to_int(roman_string);
+	if not roman_string:
+		return 0
+if not isinstance(roman_string, str):
+key = list(roman.key())
+
+num = 0
+last_num = 0 =[0]
+
+	for i in roman_string:
+		for j in key:
+			if j == i:
+				if roman.get(i) <= last_rom:
+					num += to_sustract(list_num)
+					list_num = [roman.get(i)]
+				else:
+					list_num.append(roman.get(i))
+ 				last_rom = roman.get(i)
+		num += to_subtract(list_num)
+		return (num)
+
+
+
