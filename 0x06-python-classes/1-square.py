8d26c4a445d0eb0207ea1adbf08c7ba1e5f91456
ugwu patiencechy@gmail.com <ugwupatiencechy@gmail.com>
2022-07-20 16:59:05 -0700

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,10 +6,15 @@ A module for working with squares.
 
 class Square:
     """
-    This would represents a 2D Polygon
+    This represents a 2D Polygon
     with 4 equal and perpendicular sides.
     """
-	 def __init__(self, size):
-        	super().__init__()
-        	self.__size = size
-    
+    def __init__(self, size=0):
+        super().__init__()
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        else:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size    
