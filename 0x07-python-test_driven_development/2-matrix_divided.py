4efdaf5b89b602ac4f484a67ff1c58bca4c39fc5
ugwu patiencechy@gmail.com <ugwupatiencechy@gmail.com>
2022-07-29 21:58:43 -0700

    test 2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,48 +1,32 @@
 #!/usr/bin/python3
-"""
-   module to print divide matrix
-"""
-
-
 def matrix_divided(matrix, div):
-    """
-       matrix_divided- divides all elements of a matrix and returns a new list
-       Arguments:
-           matrix (list): matrix elements
-           div (int): divider
-       Raises:
-           TypeError: if div is not a float or int
-           ZeroDivisionError: if div <= 0
-           TypeError: if matrix value is not int or float
-    """
-    if type(div) not in [int, float]:
-        raise TypeError("div must be a number")
-    elif check_row(matrix):
-        raise TypeError("Each row of matrix must have the same size")
-    elif div <= 0:
+    """function divides the provided matrix with the div param"""
+
+    result = []
+    if div == 0:
         raise ZeroDivisionError("division by zero")
-    elif len(matrix) > 0:
-        list = []
-        msg = "matrix must be a matrix (list of lists) of integers/floats"
-        for item in matrix:
-            new_list = []
-            for value in item:
-                if type(value) not in [int, float]:
-                    raise TypeError(msg)
-                else:
-                    x = "{:.2f}".format(value/div)
-                    new_list.append(float(x))
-            list.append(new_list)
-        return list
+    elif not div or (type(div) != int and type(div) != float):
+        raise TypeError("div must be a number")
+    elif len(matrix) == 0:
+        raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
 
+    for i in range(len(matrix)):
+        if type(matrix[i]) != list or type(matrix[-1]) != list:
+            raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
+        elif len(matrix[i]) != len(matrix[i - 1]):
+            raise TypeError("Each row of the matrix must have the same size")
+        elif not matrix[i]:
+            raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
 
-def check_row(matrix):
-    length = len(matrix)
-    try:
-        for i in range(0, length):
-            if (i + 1) != length:
-                if len(matrix[i]) != len(matrix[i + 1]):
-                    return True
-        return False
-    except TypeError:
-        return True
+        result.append([])
+
+        for j in matrix[i]:
+            if type(j) != int and type(j) != float:
+                raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
+            result[i].append(round(j / div, 2))
+
+    return(result)1~
