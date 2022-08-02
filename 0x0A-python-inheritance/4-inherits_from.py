6eb9238c5dc5c99ac29b2ac4afdab620d30897f0
ugwu patiencechy@gmail.com <ugwupatiencechy@gmail.com>
2022-08-01 18:17:07 -0700

    task 4 to 6

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Module to check if object is an instance of a specified class
+    Module to check if object is an instance of a specified class
 """
 
 
@@ -14,3 +14,6 @@ def inherits_from(obj, a_class):
     """
     if isinstance(obj, a_class) and type(obj) is not a_class:
         return True
+    else:
+        return False
+
